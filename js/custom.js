5a657b315aa044064bb6ff4069a85713deb51a0e
bostwiek <bostwiek@gmail.com>
2015-06-21 14:25:11 -0700

    10-4

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,20 @@
 $(document).ready(function() {
 
 	var mymy = $('#my1');
+
+	var mymyFull = $('#mymy_container');
+	var breathe = $('#breathe');
+
 	var internet = $('#internet');
 	var spongebob = $('#spongebob');
 	var projectListContainer = $('#project_list_container');
 	var projectListItem = $('.project_list_item')
+
+	var winWidth = $(window).height();
+	var winHeight = $(window).width();
+
+	var winWidthToggle = 0;
+	var winHeightToggle = 0;
 
 	mymy.click(function() {
 		$(this).text('REDRUM').delay(3200).slideUp(3500)
@@ -36,5 +46,42 @@ $(document).ready(function() {
 		$(this).addClass('list_item_idle')
 		$(this).removeClass('list_item_dance')
 	})
+
+	$(window).on("resize", function() {
+		if ($(this).width() <= 700) {
+			if (winWidthToggle == 0) {
+				winWidthToggle = 1;
+
+				breathe.fadeToggle("slow");
+				mymyFull.fadeToggle("slow");
+
+				projectListContainer.delay(1000).animate({
+					'height' : '90%'
+
+				}, 1500)
+			}
+		}
+		if ($(this).width() > 700) {
+			if (winWidthToggle == 1) {
+				winWidthToggle = 0;
+
+				breathe.delay(1000).fadeToggle("slow");
+				mymyFull.delay(1000).fadeToggle("slow");
+
+				projectListContainer.animate({
+					'height' : '30%'
+
+				}, 1500)
+			}
+		}
+ 	})
+
+
+	//when resizing window
+
+		//check if window is < 500px
+
+			//if yes, breathe + mymy fadeOut
+
 
 })
