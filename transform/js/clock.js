9aca0a5aa6f2964573089278dc7b3fa4e65e8313
bostwiek <bostwiek@gmail.com>
2015-06-23 20:58:13 -0700

    hope this doesnt break

--- transform/js/clock.js
+++ transform/js/clock.js
@@ -5,35 +5,53 @@ var six = $(".six"),
 
     secondCounter = 0;
 
-$(".next").on("click", { d: "n" }, rotate);
-$(".prev").on("click", { d: "p" }, rotate);
+$(".next").on("click", rotateN);
+$(".prev").on("click", rotateP);
 
 
-function rotate(e){
-  if(e.data.d=="n"){
-    degreesSS = degreesSS - 36;
-  }
-  if(e.data.d=="p"){
-    degreesSS = degreesSS + 36;
-  }
-  ten.css({
-	    "-webkit-transform": "rotateX("+degreesSS+"deg)",
-	    "-moz-transform": "rotateX("+degreesSS+"deg)",
-	    "-o-transform": "rotateX("+degreesSS+"deg)",
-	    "transform": "rotateX("+degreesSS+"deg)"
-  });
+function rotateN(e){
+	degreesSS = degreesSS - 36;
+ 	ten.css({
+		"-webkit-transform": "rotateX("+degreesSS+"deg)",
+		"-moz-transform": "rotateX("+degreesSS+"deg)",
+		"-o-transform": "rotateX("+degreesSS+"deg)",
+		"transform": "rotateX("+degreesSS+"deg)"
+	});
 
-  if (secondCounter == 9) {
-  	secondCounter = 0;
-  	degreesS = degreesS - 60;
-  	six.css({
-	    "-webkit-transform": "rotateX("+degreesS+"deg)",
-	    "-moz-transform": "rotateX("+degreesS+"deg)",
-	    "-o-transform": "rotateX("+degreesS+"deg)",
-	    "transform": "rotateX("+degreesS+"deg)"
-  	});
-  } else {
-  	secondCounter = secondCounter + 1;
-  }
+	if (secondCounter == 9) {
+		secondCounter = 0;
+		degreesS = degreesS - 60;
+		six.css({
+			"-webkit-transform": "rotateX("+degreesS+"deg)",
+			"-moz-transform": "rotateX("+degreesS+"deg)",
+			"-o-transform": "rotateX("+degreesS+"deg)",
+			"transform": "rotateX("+degreesS+"deg)"
+		});
+	} else {
+		secondCounter = secondCounter + 1;
+	}
+
+}
+function rotateP(e){
+	degreesSS = degreesSS + 36;
+ 	ten.css({
+		"-webkit-transform": "rotateX("+degreesSS+"deg)",
+		"-moz-transform": "rotateX("+degreesSS+"deg)",
+		"-o-transform": "rotateX("+degreesSS+"deg)",
+		"transform": "rotateX("+degreesSS+"deg)"
+	});
+
+	if (secondCounter == 0) {
+		secondCounter = 9;
+		degreesS = degreesS + 60;
+		six.css({
+			"-webkit-transform": "rotateX("+degreesS+"deg)",
+			"-moz-transform": "rotateX("+degreesS+"deg)",
+			"-o-transform": "rotateX("+degreesS+"deg)",
+			"transform": "rotateX("+degreesS+"deg)"
+		});
+	} else {
+		secondCounter = secondCounter - 1;
+	}
 
 }
