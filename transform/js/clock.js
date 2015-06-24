72a4f720760860a7ccf7890cf44e49d1b1d5db23
bostwiek <bostwiek@gmail.com>
2015-06-23 19:01:54 -0700

    ;D

--- transform/js/clock.js
+++ transform/js/clock.js
@@ -1,73 +1,21 @@
-$(document).ready(function() {
+var wheel = $(".wheel"),
+    degrees  = 0;
 
-	var btn1 = $('#btn1');
-	var btn2 = $('#btn2');
-	var btn3 = $('#btn3');
-	var btn4 = $('#btn4');
-	var btn5 = $('#btn5');
-	var btn6 = $('#btn6');
-	var btn7 = $('#btn7');
-	var btn8 = $('#btn8');
-	var btn9 = $('#btn9');
-
-	var carousel = $('#carousel');
-	var container = $('.container');
-
-	var btn1clicked = 0;
-	var btn2clicked = 0;
-	var btn3clicked = 0;
-	var btn4clicked = 0;
-	var btn5clicked = 0;
-	var btn6clicked = 0;
-	var btn7clicked = 0;
-	var btn8clicked = 0;
-	var btn9clicked = 0;
-
-	var perspectiveValue = $('#perspective');
-	var attrSubmit = $('#submit');
-
-	attrSubmit.click(function() {
-		perspectiveVal = perspectiveValue.val();
-			container.css({
-				'perspective': perspectiveVal+'px'
-			})
-	})
+$(".next").on("click", { d: "n" }, rotate);
+$(".prev").on("click", { d: "p" }, rotate);
 
 
-	btn1.click(function() {
-		carousel.removeClass();
-		carousel.addClass('show-1');
-	})
-	btn2.click(function() {
-		carousel.removeClass();
-		carousel.addClass('show-2');
-	})
-	btn3.click(function() {
-		carousel.removeClass();
-		carousel.addClass('show-3');
-	})
-	btn4.click(function() {
-		carousel.removeClass();
-		carousel.addClass('show-4');
-	})
-	btn5.click(function() {
-		carousel.removeClass();
-		carousel.addClass('show-5');
-	})
-	btn6.click(function() {
-		carousel.removeClass();
-		carousel.addClass('show-6');
-	})
-	btn7.click(function() {
-		carousel.removeClass();
-		carousel.addClass('show-7');
-	})
-	btn8.click(function() {
-		carousel.removeClass();
-		carousel.addClass('show-8');
-	})
-	btn9.click(function() {
-		carousel.removeClass();
-		carousel.addClass('show-9');
-	})
-})
+function rotate(e){
+  if(e.data.d=="n"){
+    degrees = degrees - 60;
+  }
+  if(e.data.d=="p"){
+    degrees = degrees + 60;
+  }
+  wheel.css({
+    "-webkit-transform": "rotateX("+degrees+"deg)",
+    "-moz-transform": "rotateX("+degrees+"deg)",
+    "-o-transform": "rotateX("+degrees+"deg)",
+    "transform": "rotateX("+degrees+"deg)"
+  });
+}
